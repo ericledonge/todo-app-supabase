0d94a3e39342a515e7fb5a427b33d60ec74133c2
Eric Le Donge <e.ledonge@betonprovincial.com>
2023-09-16 11:38:31 -0400

    :tada: Supabase authentication

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import "./index.css";
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+const supabase = createClient(
+  "https://duewffvwiyfyjnafrfim.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1ZXdmZnZ3aXlmeWpuYWZyZmltIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMxODgzMjEsImV4cCI6MjAwODc2NDMyMX0.WIzW1Yij8Zha8a9uzYk8h539WuYxiriuVSomnOlEuUo",
+);
+
+export default function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    return <div>Logged in!</div>;
+  }
+}
